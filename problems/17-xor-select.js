694b1e4334947b055f0e47445403193b2423e7aa
tyler <infinitespace20@gmail.com>
2022-08-10 18:21:44 -0500

    17,18,19 done

--- problems/17-xor-select.js
+++ problems/17-xor-select.js
@@ -1,38 +1,45 @@
 /*******************************************************************************
-Write a function `xorSelect` that accepts an array and two callback as arguments.
-The function should return a new array containing elements of the original array
-that result in true when passed in one of the callbacks, but not both.
 
-Examples:
+ Examples:
 
-let isEven = function(n) {
-  return n % 2 === 0;
-};
+ let isEven = function(n) {
+   return n % 2 === 0;
+  };
 
-let isPositive = function(n) {
-  return n > 0;
-};
+  let isPositive = function(n) {
+    return n > 0;
+  };
 
-console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
-// [ -2, 1, 3 ]
+  console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+  // [ -2, 1, 3 ]
 
 
-let longString = function(s) {
-  return s.length > 4;
-};
+  let longString = function(s) {
+    return s.length > 4;
+  };
 
-let startsA = function(s) {
-  return s[0] === "a";
-};
+  let startsA = function(s) {
+    return s[0] === "a";
+  };
 
-console.log(
-  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
-);
-// [ 'art', 'app', 'buttery' ]
+  console.log(
+    xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+    );
+    // [ 'art', 'app', 'buttery' ]
+    Write a function `xorSelect` that accepts an array and two callback as arguments.
+    The function should return a new array containing elements of the original array
+    that result in true when passed in one of the callbacks, but not both.
 *******************************************************************************/
 
-let xorSelect = function() {
-
+let xorSelect = function(array, cb1, cb2) {
+  let newArray = []
+  for(let i = 0; i < array.length; i++) {
+    let el = array[i]
+    if((cb1(el) || cb2(el)) && !(cb1(el) && cb2(el))) {
+      newArray.push(el)
+    }
+  }
+  return newArray
 };
 
 
